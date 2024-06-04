4ba5414996bf88c93758c4c5a91fc21638087dc3
Lennis <x@lennis.dev>
2024-06-04 19:26:59 +0200

    refactor: Add foreign key constraint to notes table in Database.java

--- app/src/main/java/dev/lennis/school/notes/Database.java
+++ app/src/main/java/dev/lennis/school/notes/Database.java
@@ -89,7 +89,7 @@ public class Database {
 
     public static void install() {
         String[] sql = {
-                "CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY, username TINYTEXT, heading text, text TEXT)",
+                "CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY, username TINYTEXT, heading text, text TEXT, FOREIGN KEY(username) REFERENCES users(username) ON DELETE CASCADE)",
                 "CREATE TABLE IF NOT EXISTS tags (noteID INTEGER, tag TINYTEXT, FOREIGN KEY(noteID) REFERENCES notes(id) ON DELETE CASCADE)",
                 "CREATE TABLE IF NOT EXISTS users (username TINYTEXT PRIMARY KEY, displayName TINYTEXT, passwordSalt TINYTEXT, passwordHash TEXT)" };
         for (String s : sql) {
