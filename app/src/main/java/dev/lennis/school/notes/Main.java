5ec658226c01d5fb940f46448daf7612a4accddf
Lennis <x@lennis.dev>
2024-06-07 12:30:36 +0200

    Add lock file to prevent multiple instances of the program (close #11)

--- app/src/main/java/dev/lennis/school/notes/Main.java
+++ app/src/main/java/dev/lennis/school/notes/Main.java
@@ -1,6 +1,34 @@
 package dev.lennis.school.notes;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        // Create a lock file
+
+        File lockFile = new File("notes.lock");
+        if (lockFile.exists()) {
+            System.out.println("Another instance of the program is already running");
+            System.exit(1);
+        }
+        try {
+            lockFile.createNewFile();
+        } catch (Exception e) {
+            System.out.println("Error creating lock file");
+            System.exit(1);
+        }
+
+        // Launch the GUI
+
+    }
+
+    static {
+        // Delete the lock file on shutdown
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            File lockFile = new File("notes.lock");
+            if (lockFile.exists()) {
+                lockFile.delete();
+            }
+        }));
     }
 }
