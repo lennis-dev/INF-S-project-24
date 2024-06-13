7da9204b522aa641cda00fd59e1eddae3bb6a1a8
Ben Mayer <info@leocodes.net>
2024-06-13 12:16:32 +0200

    Remove JForm Designer ad

--- app/src/main/java/dev/lennis/school/notes/gui/App.java
+++ app/src/main/java/dev/lennis/school/notes/gui/App.java
@@ -325,16 +325,6 @@ public class App extends JFrame {
     // ======== panel1 ========
     {
       panel1.setBorder(null);
-      panel1.setBorder(
-          new javax.swing.border.CompoundBorder(
-              new javax.swing.border.TitledBorder(
-                  new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-                  "JF\u006frmDes\u0069gner \u0045valua\u0074ion",
-                  javax.swing.border.TitledBorder.CENTER,
-                  javax.swing.border.TitledBorder.BOTTOM,
-                  new java.awt.Font("D\u0069alog", java.awt.Font.BOLD, 12),
-                  java.awt.Color.red),
-              panel1.getBorder()));
       panel1.addPropertyChangeListener(
           new java.beans.PropertyChangeListener() {
             @Override
